8c18b8123af0838784b7b79bbb227d6d6ee6f71f
AndyOverLord <andy007yan@gmail.com>
2020-01-15 07:11:34 +0000

    feat: add a text file work as database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,4 +11,16 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'Form Submitted!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_database(data)
+        return redirect("/thankyou.html")
+    else:
+        return 'Something went wrong'
+
+def write_to_database(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        content = data['content']
+        file = database.write(f'\n{email},{subject},{content}')

7b7d77fccfabe2edf8d623c85df88f283c9c3b16
AndyOverLord <andy007yan@gmail.com>
2020-01-15 04:34:06 +0000

    fix: DRY principle application and bug fix

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
-
-
-@app.route('/blog')
-def blog():
-    return 'These are my thoughts!'
+# @app.route('/blog')
+# def blog():
+#     return 'These are my thoughts!'
